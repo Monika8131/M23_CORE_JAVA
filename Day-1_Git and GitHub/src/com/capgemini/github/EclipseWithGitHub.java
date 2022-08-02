26699500c123aad1c0a7757ebeea65669d0656c3
USER <USER@DESKTOP-2R3Q0AU>
2022-08-02 07:51:13 +0530

    first commit

---
+++ Day-1_Git and GitHub/src/com/capgemini/github/EclipseWithGitHub.java
@@ -0,0 +1,10 @@
+package com.capgemini.github;
+
+public class EclipseWithGitHub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello World");
+	}
+
+}
