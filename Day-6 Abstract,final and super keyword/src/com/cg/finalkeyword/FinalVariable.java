20775a1739f6d62934fb038eb3ab3e4c995f884e
USER <USER@DESKTOP-2R3Q0AU>
2022-08-09 14:58:56 +0530

    first commit

--- Day-6 Abstract,final and super keyword/src/com/cg/finalkeyword/FinalVariable.java
+++ Day-6 Abstract,final and super keyword/src/com/cg/finalkeyword/FinalVariable.java
@@ -1,6 +1,7 @@
 package com.cg.finalkeyword;
 //Program on Final Variable
 public class FinalVariable {
+	final int FIN_VAR=45;
 	/*public void print()
 	{
 	//if any variable is declared as final we can't change the value of that particular variable
@@ -8,9 +9,10 @@ public class FinalVariable {
 	}*/
 
 	public static void main(String[] args) {
-		//FinalVariable f=new FinalVariable();
-		final int FIN_VAR=45;
-		System.out.println(FIN_VAR);
+		
+		FinalVariable f=new FinalVariable();
+		
+		System.out.println(f.FIN_VAR);
 		
 
 	}
