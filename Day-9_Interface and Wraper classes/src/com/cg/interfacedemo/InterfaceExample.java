f38edb878fab8e22e6b8566e170817bf42ca8d24
USER <USER@DESKTOP-2R3Q0AU>
2022-08-11 15:41:21 +0530

    first commit

---
+++ Day-9_Interface and Wraper classes/src/com/cg/interfacedemo/InterfaceExample.java
@@ -0,0 +1,33 @@
+package com.cg.interfacedemo;
+//Interface Example
+interface A
+{
+	//abstract method by default
+	void print();
+	void display();
+}
+class B implements A
+{
+	@Override
+	public void print() {
+		System.out.println("Technoserve India Foundation");
+	}
+	@Override
+	public void display()
+	{
+		System.out.println("Java Full Stack");
+	}
+}
+
+
+
+public class InterfaceExample {
+
+	public static void main(String[] args) {
+		B b=new B();
+		b.print();
+		b.display();
+
+	}
+
+}
---
+++ Day-9_Interface and Wraper classes/src/com/cg/interfacedemo/MultipleInheritanceUsingInterface.java
@@ -0,0 +1,32 @@
+package com.cg.interfacedemo;
+interface Father
+{
+	void property();
+}
+interface Mother
+{
+	void love();
+	
+}
+class Daughter implements Father,Mother
+{
+	@Override
+	public void love()
+	{
+		System.out.println("Mother loves their child");
+	}
+	@Override
+	public void property()
+	{
+		System.out.println("Father gives property to child");
+	}
+}
+public class MultipleInheritanceUsingInterface {
+
+	public static void main(String[] args) {
+		Daughter d=new Daughter();
+		d.love();
+		d.property();
+	}
+
+}
