99d669730a3eb16d4342432dfc01245bddcd7cad
USER <USER@DESKTOP-2R3Q0AU>
2022-08-16 14:38:37 +0530

    first commit

---
+++ Day-11_Strings,StringBuffer,StringBuilderandStringTokenizer/src/com/tns/strings/ComparisionOfString.java
@@ -0,0 +1,17 @@
+package com.tns.strings;
+
+public class ComparisionOfString {
+
+	public static void main(String[] args) {
+		//String str3="Hello";
+				@SuppressWarnings("unused")
+				String str="Hello";
+				String str1=new String("Hello");
+				String str2=new String("Hello");
+				// if we compare both the string using string literal and both the string is same than we will get true
+				System.out.println(str1==str2);
+				System.out.println(str1.equals(str2));
+
+	}
+
+}
---
+++ Day-11_Strings,StringBuffer,StringBuilderandStringTokenizer/src/com/tns/strings/StringCharMethod.java
@@ -0,0 +1,17 @@
+package com.tns.strings;
+
+import java.util.Scanner;
+
+public class StringCharMethod {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner (System.in);
+		String str1=s.nextLine();
+		System.out.println(str1.length());
+		System.out.println(str1.charAt(2));
+		s.close();
+
+
+	}
+
+}
