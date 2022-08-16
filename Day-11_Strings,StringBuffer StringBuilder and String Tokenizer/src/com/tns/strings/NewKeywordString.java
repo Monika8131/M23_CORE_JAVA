d5c2684f2360c4b00fb354d3dac0afa5108ae034
USER <USER@DESKTOP-2R3Q0AU>
2022-08-16 16:51:41 +0530

    first commit

---
+++ Day-11_Strings,StringBuffer StringBuilder and String Tokenizer/src/com/tns/strings/NewKeywordString.java
@@ -0,0 +1,14 @@
+package com.tns.strings;
+
+import java.util.Scanner;
+
+public class NewKeywordString {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		String str=new String(s.nextLine());
+		System.out.println(str);
+		s.close();
+	}
+
+}
---
+++ Day-11_Strings,StringBuffer StringBuilder and String Tokenizer/src/com/tns/strings/TokenizerString.java
@@ -0,0 +1,13 @@
+package com.tns.strings;
+
+import java.util.StringTokenizer;
+
+public class TokenizerString {
+
+	public static void main(String[] args) {
+		StringTokenizer st=new StringTokenizer("Lets try this");
+		System.out.println(st.nextToken());
+		System.out.println(st.nextToken());
+	}
+
+}
