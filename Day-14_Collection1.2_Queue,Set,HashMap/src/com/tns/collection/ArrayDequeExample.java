9ee94f33644a20b7fc43092a52c900b3e9fe7ec2
USER <USER@DESKTOP-2R3Q0AU>
2022-08-22 15:24:54 +0530

    first commit

--- Day-14_Collection1.2_Queue,Set,HashMap/src/com/tns/collection/ArrayDequeExample.java
+++ Day-14_Collection1.2_Queue,Set,HashMap/src/com/tns/collection/ArrayDequeExample.java
@@ -3,6 +3,7 @@ package com.tns.collection;
 import java.util.ArrayDeque;
 public class ArrayDequeExample {
 
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public static void main(String[] args) {
 		ArrayDeque a=new ArrayDeque();
 		a.add(11);
